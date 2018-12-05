fb716a97443ea0639391a30af07905c30c1d48ce
jrsperry <joshuasperry@gmail.com>
2018-12-05 09:42:10 -0700

    Add All Files

---
+++ IntexAzure/IntexAzure/Controllers/CustomersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IntexAzure.DAL;
+using IntexAzure.Models;
+
+namespace IntexAzure.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private IntexContext db = new IntexContext();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = db.Customer.Include(c => c.Employees);
+            return View(customer.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customer.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmpID = new SelectList(db.Employee, "EmpID", "EmpName");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustID,CustName,CustAddress1,CustAddress2,CustCity,CustState,CustZip,CustEmail,CustPaymentInfo,EmpID")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customer.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmpID = new SelectList(db.Employee, "EmpID", "EmpName", customers.EmpID);
+            return View(customers);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customer.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmpID = new SelectList(db.Employee, "EmpID", "EmpName", customers.EmpID);
+            return View(customers);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustID,CustName,CustAddress1,CustAddress2,CustCity,CustState,CustZip,CustEmail,CustPaymentInfo,EmpID")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmpID = new SelectList(db.Employee, "EmpID", "EmpName", customers.EmpID);
+            return View(customers);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customer.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customers customers = db.Customer.Find(id);
+            db.Customer.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IntexAzure/IntexAzure/DAL/IntexContext.cs
@@ -0,0 +1,23 @@
+﻿using IntexAzure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IntexAzure.DAL
+{
+    public class IntexContext : DbContext
+    {
+        public IntexContext() : base("IntexContext")
+        {
+        }
+
+        public DbSet<Customers> Customer { get; set; }
+        public DbSet<Assays> Assay { get; set; }
+        public DbSet<AssayTypes> AssayType { get; set; }
+        public DbSet<WorkOrders> WorkOrder { get; set; }
+        public DbSet<Employees> Employee { get; set; }
+
+    }
+}
